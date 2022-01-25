1a2cb9225b268c66653f90d53c0c2dae92c898ae
9HYUN <wosmd1004@naver.com>
2022-01-25 23:56:35 +0900

    100joon

---
+++ src/stage2/Number2753.java
@@ -0,0 +1,25 @@
+package stage2;
+
+import java.util.Scanner;
+
+public class Number2753 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		int score = input.nextInt();
+		
+		
+		if((score%4) == 0 && (score/100 != 0 || (score%400) == 0))
+		{
+			System.out.println(1);
+		}
+		else
+		{
+			System.out.println(0);
+		}
+		input.close();
+	}
+
+}
