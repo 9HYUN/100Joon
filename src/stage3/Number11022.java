f58d3a9584fe28b08a0bf1e85e07ed1162bb40ef
9HYUN <wosmd1004@naver.com>
2022-01-27 23:13:48 +0900

    100joon

---
+++ src/stage3/Number11022.java
@@ -0,0 +1,21 @@
+package stage3;
+
+import java.util.Scanner;
+
+public class Number11022 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int number = input.nextInt();
+		for(int j = 1; j<= number; j++)
+		{
+			int A = input.nextInt();
+			int B = input.nextInt();
+			
+			System.out.println("Case #"+ j +": "+ A +" + "+ B +" = "+ (A+B));
+		}
+		input.close();
+		
+	}
+
+}
