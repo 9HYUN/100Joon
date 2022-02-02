ac28b5eb1da31c7f7cd71fcc6ca6070b4d0ae859
9HYUN <wosmd1004@naver.com>
2022-02-02 23:24:13 +0900

    100jonn

---
+++ src/stage4_while/Number10952.java
@@ -0,0 +1,27 @@
+
+package stage4_while;
+
+import java.util.Scanner;
+
+public class Number10952 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int A;
+		int B;
+		while(true)
+		{
+			A = input.nextInt();
+			B = input.nextInt();
+			
+			if (A == 0 && B == 0)
+			{
+				input.close();
+				break;
+			}
+			System.out.println(A+B);
+		}
+	}
+
+}
