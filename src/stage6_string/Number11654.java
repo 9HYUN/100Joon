5af0f04ad083d7065a547941add47dce29c1f193
9HYUN <wosmd1004@naver.com>
2022-02-09 23:19:24 +0900

    100joon

---
+++ src/stage6_string/Number11654.java
@@ -0,0 +1,13 @@
+package stage6_string;
+
+import java.util.Scanner;
+
+public class Number11654 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int inp = input.next().charAt(0);
+		System.out.println(inp);
+	}
+
+}
